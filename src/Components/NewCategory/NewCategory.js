80dff33644abb009f29742fa4d10ec4327bb523d
ilya-sk <ilyarus@inbox.ru>
2021-03-24 20:34:27 +0300

    много багов

--- src/Components/NewCategory/NewCategory.js
+++ src/Components/NewCategory/NewCategory.js
@@ -29,7 +29,7 @@ function NewCategory(data) {
                 <div className={style.blockList}>
                     <div className={style.blockListHead}>
 
-                        {data.new.map(item => <h4 className={style.blockTitle} style={{ borderTopColor: 'blue' }}>{item.title}</h4>)};
+                        {/* {data.new.map(item => <h4 className={style.blockTitle} style={{ borderTopColor: 'blue' }}>{item.title}</h4>)}; */}
 
                         <button className={style.btnDown}><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M6 9L12 15L18 9" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
